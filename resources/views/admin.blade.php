8f9f3bccc78d760650cc1d802f115c773145306d
john <jonwanjema@gmail.com>
2020-10-05 18:47:08 +0300

    update route

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -34,7 +34,7 @@
 
         <!-- Fonts and Codebase framework -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
-        <link rel="stylesheet" id="css-main" href="assets/css/codebase.min.css">
+        <link rel="stylesheet" id="css-main" href="{{asset('assets/css/codebase.min.css')}}">
 
         <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
         <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
