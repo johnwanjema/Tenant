6fdd6b01dd373812e3d0d75a9af42be4a14dba6d
john <jonwanjema@gmail.com>
2020-10-05 18:29:25 +0300

    initial commit

---
+++ resources/js/routes/admin.js
@@ -0,0 +1,10 @@
+import Dashboard from '../components/admin/dashboard.vue';
+
+
+export default [
+    {
+        path: '/admin',
+        component: Dashboard,
+        name: 'Dashboard',
+    },
+]
